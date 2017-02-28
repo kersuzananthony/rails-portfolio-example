7688a5ff1690936b3699b479384a595bbe5b7e49
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-02-28 22:26:06 +0100

    Adds toggle status route

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -58,6 +58,23 @@ class BlogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+
+  def toggle_status
+    notice = ''
+
+    if @blog.published?
+      notice = 'Blog has been marked as draft'
+      @blog.draft!
+    else
+      notice = 'Blog has been published'
+      @blog.published!
+    end
+
+    respond_to do |format|
+      format.html { redirect_to blogs_path, notice: notice }
+      format.json { render :index, status: :ok, location: blogs_url }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status, to: 'blogs#toggle_status'
+    end
+  end
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
